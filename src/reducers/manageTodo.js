a2cf4e576ceedde4abcda1e94f2fcc800d1185ca
pawlkris <paulkristapovich@gmail.com>
2018-01-26 10:48:52 -0500

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,5 +1,16 @@
-export default function manageTodo(state = {
-  todos: [],
-}, action) {
-  return state;
+const initialState = {
+  todos: [
+    { text: 'buy groceries' },
+    { text: 'watch netflix' },
+  ]
+}
+
+export default function manageTodo(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return { todos: state.todos.concat(action.todo.text) };
+
+    default:
+      return state;
+    }
 }
